daf3b6c1bf0fe5c77156c38d3b00afca4dd34011
Saptodeep Sarkar <saptodeepsarkar28@gmail.com>
2025-11-04 21:31:35 +0530

    Ip Issues Fixed

--- server/index.ts
+++ server/index.ts
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // This serves both the API and the client.
   const port = parseInt(process.env.PORT || '5000', 10);
-  const host = process.env.HOST || "127.0.0.1";
+  const host = process.env.HOST || "0.0.0.0";
 
   server.listen(port, host, () => {
     log(`Serving on http://${host}:${port}`);
--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     port: 5000,
     fs: {
       strict: true,
